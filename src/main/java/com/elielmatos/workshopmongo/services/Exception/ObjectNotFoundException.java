94117f0ec76a054f70f9226f4a819e0bb127a7cb
Eliel Matos <eliellimamatos@gmail.com>
2021-06-19 20:54:59 -0400

    Obtendo usuario por ID

---
+++ src/main/java/com/elielmatos/workshopmongo/services/Exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.elielmatos.workshopmongo.services.Exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
--- src/main/java/com/elielmatos/workshopmongo/services/UserService.java
+++ src/main/java/com/elielmatos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.elielmatos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.elielmatos.workshopmongo.domain.User;
 import com.elielmatos.workshopmongo.repository.UserRepository;
+import com.elielmatos.workshopmongo.services.Exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,9 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
